fb6803c429f292c2e82a2825027f77142afbdfa2
matlorr <matvey.lorkish@outlook.de>
2026-06-19 14:32:13 +0200

    Adjusted and fixed tests for hardware measurement

--- Usage/measurement/measurement_test.py
+++ Usage/measurement/measurement_test.py
@@ -1,5 +1,3 @@
-import json
-import difflib
 from unittest import TestCase, main
 from unittest.mock import patch
 import pandas
@@ -15,9 +13,9 @@ class TestMeasurement(TestCase):
         #self.doc_df = pandas.read_csv("./test_files/doc_meas.csv", delimiter=';', index_col=False)
         self.expected_empty_documentation = "Usage/measurement/test_files/empty_hardware_measurement.csv"
         self.new_hardware_measurement = "Usage/measurement/test_files/new_hardware_measurement.csv"
-        self.new_user_measurement = "Usage/measurement/test_files/new_user_measurement.csv"
+        self.new_user_measurement = "test_files/new_user_measurement.csv"
 
-        self.exp_ft_combined_measurement = "Usage/measurement/test_files/expected_first_time_combined_measurement.csv"
+        self.exp_ft_combined_measurement = "test_files/expected_first_time_combined_measurement.csv"
 
         self.empty_doc_df = pandas.DataFrame({"Timestamp": [], 
                                               "CPU": [], 
@@ -30,38 +28,30 @@ class TestMeasurement(TestCase):
 
         #self.expected_initialized_documentation_without_home_page_access = "./test_files/exp_empty_doc_new_with_home_page_access_non_shuffled.csv"
 
-    @patch('measurement.UsageMeter.get_measurement')
-    def test_measuring_hardware_with_empty_documented_measurements(self, mock_get_measurement):
-        empty_doc_df = pandas.read_csv(self.expected_empty_documentation, index_col=False)
-        
-        mock_data = pandas.read_csv(self.new_hardware_measurement)
-        mock_get_measurement.return_value = mock_data
-
-        res_df = self.um.measure_hardware(empty_doc_df)
-
-        self.assertTrue(mock_data.equals(res_df))
     
+    @patch('measurement.UsageMeter.get_max_cpu_usage_over_ten_min')
+    @patch('measurement.UsageMeter.get_max_ram_usage_over_ten_min')
     @patch('measurement.UsageMeter.get_timestamp_now')
-    def test_first_time_updating_combined_user_and_hardware_measurements(self, mock_get_timestamp_now):
+    def test_first_time_updating_combined_user_and_hardware_measurements(self, 
+                                                                         mock_get_timestamp_now,
+                                                                         mock_get_max_ram_usage_over_ten_min,
+                                                                         mock_get_max_cpu_usage_over_ten_min):
         exp_df = pandas.read_csv(self.exp_ft_combined_measurement)
         exp_df = self.um.apply_measurement_dtypes(exp_df)
-        sbs_hardware = pandas.read_csv(self.new_hardware_measurement)
         sbs_users = pandas.read_csv(self.new_user_measurement)
 
+        mock_get_max_cpu_usage_over_ten_min.return_value = 0.98924
+        mock_get_max_ram_usage_over_ten_min.return_value = 0.82792
+        mock_get_timestamp_now.return_value = pandas.Timestamp(year=2026, month=1, day=22, hour=12, minute=10)
 
-        mock_timestamp = pandas.Timestamp(year=2026, month=1, day=22, hour=12, minute=10)
-        mock_get_timestamp_now.return_value = mock_timestamp
-
-        daily_hardware_user_log: pandas.DataFrame = pandas.DataFrame({"Timestamp": [], 
-                                                          "Max_usr": [], 
-                                                          "Max_cpu": [],
-                                                          "Max_mem": []})
+        daily_hardware_user_log: pandas.DataFrame = pandas.DataFrame({
+            "Timestamp": [], 
+            "Max_usr": [], 
+            "Max_cpu": [],
+            "Max_mem": []})
         
         res_df = self.um.update_measurements(daily_hardware_user_log,
-                                             sbs_hardware, 
                                              sbs_users)
-
-        print(res_df["Timestamp"][0] == exp_df["Timestamp"][0])
 
         self.assertTrue(exp_df.equals(res_df))
     

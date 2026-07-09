3f9e60329312187009833fdf33aed5c74b680587
matlorr <matvey.lorkish@outlook.de>
2026-07-09 16:29:29 +0200

    Externalize calculation of ten minute maxima to dedicated classes.

--- Usage/measurement/measurement.py
+++ Usage/measurement/measurement.py
@@ -15,6 +15,10 @@ import requests
 
 from io import StringIO
 
+from Location.measurement.measurement import LocationMeter
+from Usage.measurement.cpu_meter import CpuMeter
+from Usage.measurement.ram_meter import RamMeter
+
 HOME_PAGE_GAMES = ['leanprover-community/nng4',
                    'hhu-adam/robo',
                    'djvelleman/stg4',
@@ -37,60 +41,30 @@ class UsageMeter:
         #self.HARDWARE_SCRIPT = os.environ.get("HARDWARE_SCRIPT")
         self.hardware_info_file = os.environ.get("HARDWARE_INFO_FILE")
         self.prom_con = PrometheusConnect(url="http://localhost:9090", disable_ssl=True)
-    
-    def get_max_ram_usage_over_ten_min(self) -> float:
-        ram_query = """
-        (1 - min_over_time(
-            (node_memory_MemAvailable_bytes / node_memory_MemTotal_bytes)
-            [10m:]
-        )) * 100
-        """
-
-        result_list = self.prom_con.custom_query(ram_query)
-        assert len(result_list) == 1, f"[UsageMeter] Expected 1 result, got {len(result_list)}"
-        ram_result = result_list[0]
-
-        instance = ram_result["metric"].get("instance", "unknown")
-        max_ram = float(ram_result["value"][1])
-        print(f"[RAM] Instance: {instance} | Max usage: {max_ram:.2f}%")
-        return max_ram
-    
-    def get_max_cpu_usage_over_ten_min(self) -> float:
-        # 1) Count the amount of seconds each CPU is in idle-mode
-        # 2) Compute with rate the idle-fraction per core, smoothed over a minute
-        # 3) Average the idle-rates over all cores
-        # 4) Compile minimum average idle-rate of the last then minutes
-        # 5) Subtract minimum average idle-rate from 1 and multiply by 100 
-        # to get maximum average usage for the last ten minutes
-        cpu_query = """
-        (1 - min_over_time(
-            avg by(instance) (rate(node_cpu_seconds_total{mode="idle"}[1m]))
-            [10m:]
-        )) * 100
-        """
-
-        result_list = self.prom_con.custom_query(cpu_query)
-        assert len(result_list) == 1, f"[UsageMeter] Expected 1 result, got {len(result_list)}"
-        cpu_result = result_list[0]
-
-        instance = cpu_result["metric"].get("instance", "unknown")
-        max_cpu = float(cpu_result["value"][1])
-        print(f"[CPU] Instance: {instance} | Max usage: {max_cpu:.2f}%")
-        return max_cpu
-
-    
+        # Zero-initialize list of 40 15-sec idle measurements
+        # this list corresponds to all idle percentages over the 
+        # last ten minutes.
+        #self.cpu_idle_percentages = [0.0]*40
+        #self.old_avg_idle_time = 0.0
+        self.cpu_meter = CpuMeter(prometheus_connection=self.prom_con)
+        self.ram_meter = RamMeter(prometheus_connection=self.prom_con)
+        self.loc_meter = LocationMeter()
+            
     def update_measurements(self,
-                            doc_measurements: pandas.DataFrame, 
-                            sbs_users: pandas.DataFrame) -> pandas.DataFrame:
+                            doc_measurements: pandas.DataFrame) -> pandas.DataFrame:
                 
-        result = self.get_measurement(sbs_users)
+        result = self.get_measurement()
         print(f"[{datetime.datetime.now()}] Updated user-hardware log.")
         return pandas.concat([doc_measurements, result])
 
-    def get_measurement(self, sbs_users):
-        max_cpu = self.get_max_cpu_usage_over_ten_min()
-        max_mem = self.get_max_ram_usage_over_ten_min()
-        max_usr = sbs_users['Users'].max()
+    def update_cpu_measurement(self):
+        self.cpu_meter.update_cpu_idle_percentages()
+
+    def get_measurement(self):
+        max_cpu = self.cpu_meter.get_max_cpu_usage_over_last_ten_min()
+        max_mem = self.ram_meter.get_max_ram_usage_over_ten_min()
+        # max_usr = sbs_users['Users'].max()
+        max_usr = self.loc_meter.get_max_users_over_ten_minutes()
         print(f"[USERS] Max users: {max_usr}")
         timestamp = self.get_timestamp_now()
         print(f"Timestamp: {timestamp}")

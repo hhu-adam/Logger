64108733fdbcd27e06cc8b3185818cec5062ec27
matlorr <matvey.lorkish@outlook.de>
2026-07-09 16:24:41 +0200

    Introduce RamMeter class to retrieve the maximum ram use over the last ten minutes.

---
+++ Usage/measurement/ram_meter.py
@@ -0,0 +1,22 @@
+from prometheus_api_client.prometheus_connect import PrometheusConnect
+
+class RamMeter:
+    def __init__(self, prometheus_connection: PrometheusConnect) -> None:
+        self.prom_con = prometheus_connection
+
+    def get_max_ram_usage_over_ten_min(self) -> float:
+        ram_query = """
+        (1 - min_over_time(
+            (node_memory_MemAvailable_bytes / node_memory_MemTotal_bytes)
+            [10m:]
+        )) * 100
+        """
+
+        result_list = self.prom_con.custom_query(ram_query)
+        assert len(result_list) == 1, f"[UsageMeter] Expected 1 result, got {len(result_list)}"
+        ram_result = result_list[0]
+
+        instance = ram_result["metric"].get("instance", "unknown")
+        max_ram = float(ram_result["value"][1])
+        print(f"[RAM] Instance: {instance} | Max usage: {max_ram:.2f}%")
+        return max_ram
